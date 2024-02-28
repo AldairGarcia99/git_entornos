e11767b96fdbee0278e6d69593327b605d313dc5
unknown <aldairgarciap15@gmail.com>
2024-02-28 19:57:36 +0100

    paso7

--- archivo1.java
+++ archivo1.java
@@ -10,6 +10,7 @@ public class archivo1 {
 		System.out.println("hola lucia");
 		System.out.println("hola jorge");
 		System.out.println("hola manuel");
+		System.out.println("hola mario");
 	}
 
 }
